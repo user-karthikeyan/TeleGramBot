86621ab018e24a7c105ee42fd77c8463312085f2
user-karthikeyan <89862681+user-karthikeyan@users.noreply.github.com>
2023-10-04 11:17:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 if __name__ == '__main__':
     getMessage("CIT-1.pdf")
-    application = ApplicationBuilder().token('6546583318:AAHbcZTbx0VVkeFV9OQQM6UswI4QUIfkZBo').build()
+    application = ApplicationBuilder().token(os.getenv("Key")).build()
     
     #Building bot on the given ID
 
